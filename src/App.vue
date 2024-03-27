1145a96bf91336eb5b906efc4cbc536a641c6284
Andrey Luzan <andrejluzan@gmail.com>
2024-03-27 21:05:59 +0700

    Инициализация проекта

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1 class="app__title">Plum Bucket - магазин топовой электроники</h1>
+  <AppLayout></AppLayout>
+</template>
+
+<script setup>
+// Components
+import AppLayout from './layouts/AppLayout.vue';
+</script>
+
+<style>
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+
+.visually-hidden {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+  border: 0;
+  padding: 0;
+  white-space: nowrap;
+  clip-path: inset(100%);
+  clip: rect(0 0 0 0);
+  overflow: hidden;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: 'Arial', sans-serif;
+}
+
+.app__title {
+  display: none;
+}
+</style>
