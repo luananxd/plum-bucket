7ccd7d19f2ff5b0254cc17fa3f83327c5189b476
Andrey Luzan <andrejluzan@gmail.com>
2024-03-30 21:14:58 +0700

    Регистрация глобальных компонентов

---
+++ src/components/atomic/index.js
@@ -0,0 +1,13 @@
+import AppButton from './AppButton.vue';
+import AppSelect from './AppSelect.vue';
+
+export const globalComponents = [
+  {
+    name: 'AppButton',
+    source: AppButton,
+  },
+  {
+    name: 'AppSelect',
+    source: AppSelect,
+  },
+]
